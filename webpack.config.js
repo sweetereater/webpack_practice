091c5f7649262d6ae0e45952c8e1a997a0c0b1a9
sweetereater <sweeter_2093@mail.ru>
2024-12-12 22:47:10 +0300

    added 2 build scripts (:dev and :prod) + added env variables in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
 
-module.exports = {
-  // Режим сборки => 'development' | 'production' | 'none'
-  mode: 'production',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    // Имя бандла, [contenthash] рассчитывается с учетом содержимое => меняется код - меняется имя сборки
-    // [contenthash] используется для того, чтобы при изменениях в сборке - браузер брал данные
-    filename: 'bundle.[contenthash].js',
-    // Путь до папки с итоговой сборкой
-    path: path.resolve(__dirname, 'build'),
-    // Очистка папки от старых бандлов
-    clean: true,
+module.exports = (env) => {
+  return {
+    // Режим сборки => 'development' | 'production' | 'none'
+    mode: env.mode,
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      // Имя бандла, [contenthash] рассчитывается с учетом содержимое => меняется код - меняется имя сборки
+      // [contenthash] используется для того, чтобы при изменениях в сборке - браузер брал данные
+      filename: 'bundle.[contenthash].js',
+      // Путь до папки с итоговой сборкой
+      path: path.resolve(__dirname, 'build'),
+      // Очистка папки от старых бандлов
+      clean: true,
+    }
   }
 }
