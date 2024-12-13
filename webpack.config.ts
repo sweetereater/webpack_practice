830270620f6c7ed9b46d51cae574bafbeea0b9d3
sweetereater <sweeter_2093@mail.ru>
2024-12-13 15:26:33 +0300

    aded scss support

--- webpack.config.ts
+++ webpack.config.ts
@@ -48,8 +48,15 @@ export default (env: ConfigEnv): webpack.Configuration => {
           exclude: /node_modules/,
         },
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
         },
       ],
     },
