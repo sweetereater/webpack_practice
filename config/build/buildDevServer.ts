ca921d57f8021a3b2c2ab02d879e7f649b3256b6
sweetereater <sweeter_2093@mail.ru>
2024-12-15 23:25:21 +0300

    added historyApiFallback: true for routing (only for dev server)

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,6 +3,7 @@ import type { Configuration as DevServerConfiguration } from "webpack-dev-server
 
 function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
+    historyApiFallback: true,
     open: true,
     port: options.port ?? 3000,
   }
