b0ed986d2238d476d65f3bf08dc46810d01d7a6c
sweetereater <sweeter_2093@mail.ru>
2024-12-15 23:24:29 +0300

    install react-router-dom and added simple router in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,28 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = document.getElementById('root');
 
 const container = createRoot(root);
 
-container.render(<App />);
+// Никогда раньше не пользовался createBrowserRouter :o
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/about",
+        element: <h1>About page</h1>,
+      },
+      {
+        path: "/info",
+        element: <h1>Info page</h1>,
+      },
+    ],
+  },
+]);
+
+
+container.render(<RouterProvider router={router} />);
